a4bd87c309006c3b3e3555e52a5a1fcd25aa1b81
Omkar38-hub <68990485+Omkar38-hub@users.noreply.github.com>
2021-09-17 08:40:14 +0530

    All the required file created and content added in that

---
+++ index.js
@@ -0,0 +1,22 @@
+var button=document.querySelector(".click");
+var text=document.querySelector(".input");
+var urlSelection="https://api.funtranslations.com/translate/ferb-latin.json";
+// attaching text with url
+function createApi(input_given)
+{
+  return urlSelection+"?"+"text="+input_given;
+}
+// function for error handling
+function errorHandling(error)
+{
+  console.log("Error"+error);
+  alert("Something went wrong!.Please try again later.")
+}
+button.addEventListener("click",function(){
+   fetch(createApi(text.value))
+   .then(response=>response.json())
+   .then(json=>{
+     var translatedText=json.contents.translated;
+     document.querySelector(".output").innerText=translatedText;
+   }).catch(errorHandling)
+})
