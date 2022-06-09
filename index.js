f2dbdd3bc2274a8bb1519edd6584f013a767cd67
Omkar38-hub <68990485+Omkar38-hub@users.noreply.github.com>
2022-06-09 13:20:42 +0530

    dropdown of various language added

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 var button=document.querySelector(".click");
 var text=document.querySelector(".input");
-var urlSelection="https://api.funtranslations.com/translate/ferb-latin.json";
+var urlSelection="https://api.funtranslations.com/translate/minion.json";
 // attaching text with url
+
+function getCharacter(){
+    var element=document.getElementById("choose").value;
+    if(element==1)
+    {
+        console.log("minions");
+        urlSelection="https://api.funtranslations.com/translate/minion.json";
+    }
+    else if(element==2)
+    {
+        console.log("Shakespeare");
+        urlSelection="https://api.funtranslations.com/translate/shakespeare.json"
+    }
+    else if(element==3)
+    {
+        console.log("Oldenglish");
+        urlSelection="https://api.funtranslations.com/translate/oldenglish.json"
+    }
+    else if(element==4)
+    {
+        console.log("Valspeak");
+        urlSelection="https://api.funtranslations.com/translate/valspeak.json";
+
+    }
+    else if(element==5)
+    {
+        console.log("Yoda");
+        urlSelection="https://api.funtranslations.com/translate/yoda.json"
+    }
+}
 function createApi(input_given)
 {
   return urlSelection+"?"+"text="+input_given;
